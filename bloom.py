de79d98416ee171ef795534af98ed048d85764f4
barche <f.blaise94@yahoo.com>
2024-06-21 14:23:19 +0200

    initialilize custom hash function class

---
+++ bloom.py
@@ -0,0 +1,7 @@
+class CustomBloomFilterHashFunctions:
+    """
+    A class to implement custom hash functions for Bloom Filters.
+    """
+    def __init__(self, size: int):
+        self.size = size
+    
